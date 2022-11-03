2d73d64a4b85859ebcb4e4294f09ff828f5c8976
WBJ-000337 <emily.batista@webjump.com.br>
2022-11-03 17:19:27 -0300

    cenários

--- cypress/integration/magento.spec.js
+++ cypress/integration/magento.spec.js
@@ -4,4 +4,10 @@ describe('Magento', () => {
   it('Checks for the initial state', () => {
     cy.percySnapshot();
   });
+
+  it('Checks for invalid email', ()=>{
+    cy.get('#email_address').type('teste##*');
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click();
+    cy.percySnapshot();
+  })
 });
