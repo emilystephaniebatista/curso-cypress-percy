fa512bbbf95be5855ee86b4b9fa4605c62fc172c
WBJ-000337 <emily.batista@webjump.com.br>
2022-11-03 17:40:42 -0300

    cenários

--- cypress/integration/magento.spec.js
+++ cypress/integration/magento.spec.js
@@ -10,4 +10,12 @@ describe('Magento', () => {
     cy.get('#form-validate > .actions-toolbar > div.primary > .action').click();
     cy.percySnapshot();
   })
+  it('Enable submission after all mandatory fields are filled', () => {
+    cy.get('#firstname').type('Emily');
+    cy.get('#lastname').type('Batista');
+    cy.get('#email_address').type('teste1238@hotmail.com');
+    cy.get('#password').type('teste1234@');
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click();
+    cy.percySnapshot();
+  });
 });
